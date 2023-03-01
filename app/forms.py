2bc169c4dc82f96820c979cdbcfb1ded0a2d77df
Chad Nguyen <chadn458@gmail.com>
2023-03-01 10:12:06 -0500

    carsale

---
+++ app/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Email
+
+class SignUpForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match!')])
+    username = StringField('Username', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class CarForm(FlaskForm):
+    make = StringField('Make', validators=[DataRequired()])
+    model = StringField('Model', validators=[DataRequired()])
+    year = StringField('Year', validators=[DataRequired()])
+    color = StringField('Color', validators=[DataRequired()])
+    price = StringField('Price', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,38 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import SignUpForm, LoginForm, CarForm
 
 @app.route('/')
 def index():
-    cdn={
-        'instructors':('lucas','dylan'),
-        'students':['blane','ashmika','abe','zi','connor','martin','noah','erm']
-    }
-    return render_template('index.jinja', cdn=cdn, title='Home')
+    return render_template('index.jinja', title='Home')
 
-@app.route('/login')
-def login():
-    return render_template('login.jinja')
+@app.route('/login', methods=['GET', 'POST'])
+def show_login_form():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        flash(f'{login_form.username} successfully signed in!')
+        return redirect('/')
+    return render_template('login.jinja', login_form=login_form)
 
-@app.route('/register')
-def register():
-    return render_template('register.jinja')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    signup_form = SignUpForm()
+    if signup_form.validate_on_submit():
+        flash(f'Request to signup {signup_form.username.data} successful')
+        return redirect('/')
+    return render_template('signup.jinja', form=signup_form)
 
+@app.route('/carform', methods=['GET', 'POST'])
+def show_car_form():
+    car_form = CarForm()
+    return render_template('car.jinja', car_form=car_form)
 
 @app.route('/about')
 def about():
     return render_template('about.jinja')
 
-
 @app.route('/blog')
 def blog():
     return render_template('blog.jinja')
+
+
